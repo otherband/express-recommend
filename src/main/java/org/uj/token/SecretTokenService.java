887fa44075f55a16b442be24156d29c6368ea102
Yazan Amer <yazan.amer@progressoft.com>
2023-09-27 16:11:42 +0300

    tokens

--- src/main/java/org/uj/token/SecretTokenService.java
+++ src/main/java/org/uj/token/SecretTokenService.java
@@ -1,5 +1,12 @@
 package org.uj.token;
 
+import java.time.LocalDateTime;
+
 public class SecretTokenService {
 
+    public SecretToken create() {
+        SecretToken secretToken = new SecretToken();
+        secretToken.setCreationDate(LocalDateTime.now());
+        return secretToken;
+    }
 }
--- src/test/java/org/uj/token/SecretTokenServiceTest.java
+++ src/test/java/org/uj/token/SecretTokenServiceTest.java
@@ -1,5 +1,24 @@
 package org.uj.token;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class SecretTokenServiceTest {
-    private SecretTokenService tokenService;
+    private SecretTokenService tokenService = new SecretTokenService();
+
+    @Test
+    void create() {
+        SecretToken secretToken = tokenService.create();
+        assertAlmostNow(secretToken.getCreationDate());
+    }
+
+    private void assertAlmostNow(LocalDateTime creationDate) {
+        assertThat(Duration.between(creationDate, LocalDateTime.now()).toMillis())
+                .isLessThan(100);
+    }
+
 }
