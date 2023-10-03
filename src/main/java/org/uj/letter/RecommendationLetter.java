86a2dd997bea0932cbac4cfcd363c425fea1b48f
Yazan Amer <yazan.amer@progressoft.com>
2023-10-03 08:05:27 +0300

    JPA infrastructure

--- src/main/java/org/uj/letter/RecommendationLetter.java
+++ src/main/java/org/uj/letter/RecommendationLetter.java
@@ -3,10 +3,12 @@ package org.uj.letter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "recommendation_letter")
 public class RecommendationLetter {
     @Id
     @Column(name = "id", nullable = false, unique = true)
