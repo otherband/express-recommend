e9833cfa492f0708ad88496dc5b1f0de39159712
Yazan Amer <yazan.amer@progressoft.com>
2023-10-04 09:21:20 +0300

    dev email service

--- src/main/java/org/uj/email/EmailServiceSimulator.java
+++ src/main/java/org/uj/email/EmailServiceSimulator.java
@@ -1,11 +1,14 @@
 package org.uj.email;
 
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("dev")
+@Component
 public class EmailServiceSimulator implements EmailService {
     @Override
     public void sendEmail(String receivedAddress, String title, String body) {
+
 
     }
 }
