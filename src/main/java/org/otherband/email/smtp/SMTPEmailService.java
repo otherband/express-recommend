4a626d02bd359ff947b5828865b31d5f9f46eed5
Malek Adawi <malek_adawi@protonmail.com>
2023-10-07 22:13:45 +0300

    add javaMailSender bean

--- src/main/java/org/otherband/email/smtp/SMTPEmailService.java
+++ src/main/java/org/otherband/email/smtp/SMTPEmailService.java
@@ -9,7 +9,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-@Profile("production")
+@Profile("prod")
 @Service
 public class SMTPEmailService implements EmailService {
     private final String mailUserName;
