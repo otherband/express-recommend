dee388a0b8ac9e620867afe231e6b9862f40470f
Yazan Amer <yazan.amer@progressoft.com>
2023-10-10 13:00:27 +0300

    refactor: tests

--- src/test/java/org/otherband/BaseJpaTest.java
+++
@@ -1,7 +0,0 @@
-package org.otherband;
-
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@DataJpaTest
-public class BaseJpaTest {
-}
--- src/test/java/org/otherband/letter/RecommendationLetterControllerTest.java
+++ src/test/java/org/otherband/letter/RecommendationLetterControllerTest.java
@@ -1,26 +1,24 @@
 package org.otherband.letter;
 
 import org.junit.jupiter.api.Test;
+import org.otherband.BaseApplicationTest;
+import org.otherband.FakeEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.otherband.BaseApplicationTest;
-import org.otherband.FakeEmailService;
 
 import java.io.UnsupportedEncodingException;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.otherband.letter.RecommendationLetterController.LETTER_ENDPOINT;
+import static org.otherband.letter.RecommendationLetterController.RecommendationLetterRequest;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.otherband.letter.RecommendationLetterController.LETTER_ENDPOINT;
-import static org.otherband.letter.RecommendationLetterController.RecommendationLetterRequest;
 
-@SpringBootTest
 public class RecommendationLetterControllerTest extends BaseApplicationTest {
 
     public static final String VALID_ID = "VALID_ID";
